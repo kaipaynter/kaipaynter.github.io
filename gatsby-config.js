8bbf4e81ce5c019932a50337fa9612c2e474e9d9
beepa <admin@beartigerproductions.com>
2021-07-16 15:38:07 +0200

    gatsby config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,73 @@
 module.exports = {
   siteMetadata: {
-    title: "Kai Paynter Advisory",
-    author: "Boipelo Mawasha",
-    description: "Kai Paynter Advisory"
+    title: 'Kai Paynter Advisory',
+    titleAlt: 'Kai Paynter Advisory',
+    titleTemplate: "%s · American Accent",
+    author: 'Boipelo Mawasha',
+    description: 'Kai Paynter Advisory',
+    url: "https://kaipaynter.com",
+    image: "/images/KP-logo.jpg",
+    twitterUsername: '@Kaipadvisory',
   },
+  //pathPrefix: `/staging`,
   plugins: [
+    {
+      resolve: `gatsby-plugin-segment-js`,
+      options: {
+        // your segment write key for your production environment
+        // when process.env.NODE_ENV === 'production'
+        // required; non-empty string
+        prodKey: `sSJjPElwAHJY1wJPACOprjVf1LQnsHug`,
+  
+        // if you have a development env for your segment account, paste that key here
+        // when process.env.NODE_ENV === 'development'
+        // optional; non-empty string
+        devKey: `sSJjPElwAHJY1wJPACOprjVf1LQnsHug`,
+  
+        // boolean (defaults to false) on whether you want
+        // to include analytics.page() automatically
+        // if false, see below on how to track pageviews manually
+        trackPage: true,
+  
+        // number (defaults to 50); time to wait after a route update before it should
+        // track the page change, to implement this, make sure your `trackPage` property is set to `true`
+        trackPageDelay: 50,
+  
+        // If you need to proxy events through a custom endpoint,
+        // add a `host` property (defaults to https://cdn.segment.io)
+        // Segment docs:
+        //   - https://segment.com/docs/connections/sources/custom-domains
+        //   - https://segment.com/docs/connections/sources/catalog/libraries/website/javascript/#proxy
+        host: `https://cdn.segment.com`,
+  
+        // boolean (defaults to false); whether to delay load Segment
+        // ADVANCED FEATURE: only use if you leverage client-side routing (ie, Gatsby <Link>)
+        // This feature will force Segment to load _after_ either a page routing change
+        // or user scroll, whichever comes first. This delay time is controlled by
+        // `delayLoadTime` setting. This feature is used to help improve your website's
+        // TTI (for SEO, UX, etc).  See links below for more info.
+        // NOTE: But if you are using server-side routing and enable this feature,
+        // Segment will never load (because although client-side routing does not do
+        // a full page refresh, server-side routing does, thereby preventing Segment
+        // from ever loading).
+        // See here for more context:
+        // GIF: https://github.com/benjaminhoffman/gatsby-plugin-segment-js/pull/19#issuecomment-559569483
+        // TTI: https://github.com/GoogleChrome/lighthouse/blob/master/docs/scoring.md#performance
+        // Problem/solution: https://marketingexamples.com/seo/performance
+        delayLoad: false,
+  
+        // number (default to 1000); time to wait after scroll or route change
+        // To be used when `delayLoad` is set to `true`
+        delayLoadTime: 1000,
+  
+        // Whether to completely skip calling `analytics.load()`.
+        // ADVANCED FEATURE: only use if you are calling `analytics.load()` manually
+        // elsewhere in your code or are using a library
+        // like: https://github.com/segmentio/consent-manager that will call it for you.
+        // Useful for only loading the tracking script once a user has opted in to being tracked, for example.
+        manualLoad: false
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
@@ -17,8 +80,20 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/assets/images/logo2.png', // This path is relative to the root of the site.
       },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/carousel`, // wherever background images are stored
+      },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Oxygen`],
+        display: 'swap',
+      },
+    },
   ],
 }
