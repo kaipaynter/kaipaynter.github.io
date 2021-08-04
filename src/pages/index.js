785c1a148d88700fcbc385ae7a0426e1612ff543
beepa <admin@beartigerproductions.com>
2021-08-04 14:08:03 +0200

    fix title

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ class Index extends React.Component {
     return (
       <Layout>
         <Helmet>
-          <title>title="Kai Paynter Advisory"</title>
+          <title>Kai Paynter Advisory</title>
           <meta name='description' content='Home page' />
           <link rel="canonical" href="https://kaipaynter.com" />
         </Helmet>
