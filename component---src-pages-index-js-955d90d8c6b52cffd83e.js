8e80448f5f89376812392f11f3e5bec24a98b329
beepa <admin@beartigerproductions.com>
2021-08-11 09:33:48 +0200

    Updates

---
+++ component---src-pages-index-js-955d90d8c6b52cffd83e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{RXBc:function(n,t,c){"use strict";c.r(t);var e=c("q1tI"),o=c("Wbzz");t.default=function(){return Object(e.useEffect)((function(){Object(o.navigate)("coming-soon")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-index-js-955d90d8c6b52cffd83e.js.map
--- sw.js
+++ sw.js
@@ -27,23 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-32fb47d70606f94145ae.js"
+    "url": "webpack-runtime-f7c10cbc46f734a557aa.js"
   },
   {
-    "url": "framework-305b3707783ccc9d7ca6.js"
+    "url": "framework-beda7bde9d7dca0755ce.js"
   },
   {
-    "url": "app-9278d657ece8a0504fa0.js"
+    "url": "app-8d5905ba4efdf2ac937f.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a7a89b3503e2aa2d05066577529ad647"
+    "revision": "553f7823b4f1d6667c5eba4616ddc811"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
   },
   {
-    "url": "polyfill-d47b21a5737d9524fa70.js"
+    "url": "polyfill-aa31a558d1e59c5d37ec.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-9278d657ece8a0504fa0.js`))) {
+  if (!resources || !(await caches.match(`/app-8d5905ba4efdf2ac937f.js`))) {
     return await fetch(event.request)
   }
 
