1d6b66810ebf92b8f93f94a504b134c04563df9e
Boipelo <56187103+boipelomawasha-pers@users.noreply.github.com>
2026-08-14 07:39:21 +0000

    Refactor Gatsby configuration and update Sass color functions
    - Removed 'gatsby-plugin-react-helmet' from gatsby-config.js.
    - Added build output logs in 'build-output.log' and 'build-output.txt'.
    - Updated typography styles to use 'sass:color' for color scaling.
    - Refactored button, icon, footer, header, and menu styles to utilize 'sass:color' for color manipulations.
    - Enhanced breakpoint and function libraries to use new Sass string and map functions.

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,6 @@ module.exports = {
     },
     'gatsby-plugin-sitemap',
     'gatsby-plugin-robots-txt',
-    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
