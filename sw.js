c68eb6cab2e1e6bd29e4ddbd7fa09d26e2bc5953
boipelomawasha-pers <boipelomawasha-pers@users.noreply.github.com>
2026-08-14 06:56:15 +0000

    deploy: 77fc70f486ba92a817e2fe89df3825a6e152966e

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0fd6414f1134f19c65464c607ed3ae33"
+    "revision": "7cb399666962e32c822578323b5aa511"
   },
   {
     "url": "manifest.webmanifest",
